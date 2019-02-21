847702c86ca15c1e2e1baf20380c8f86ffb948f8
samratluitel <ngc2058@gmail.com>
2019-02-22 00:09:08 +0545

    Package-lock.json deleted for fixing hosting issue

--- client/src/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  cloudinary: {
-    APIkey: "877689714429335",
-    APISecret: "_C0YOhrLVHjAmTKMdqaJRLSGdBE",
-    uploadPreset: "ecommerce",
-    cloudName: "samrat"
-  }
-};
--- client/src/config/keys.js
+++
@@ -1,5 +0,0 @@
-if (process.env === "production") {
-  module.exports = require("./prod.js");
-} else {
-  module.exports = require("./dev.js");
-}
